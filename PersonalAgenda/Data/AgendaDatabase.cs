d61c36c27a52eb7fd86210d918640772fe720826
ROBERT VINKLER <robert.vinkler@stud.ubbcluj.ro>
2020-12-11 17:09:02 +0200

    Add project files.

---
+++ PersonalAgenda/Data/AgendaDatabase.cs
@@ -0,0 +1,45 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PersonalAgenda.Models;
+using System.Text;
+
+namespace PersonalAgenda.Data
+{
+    public class AgendaDatabase
+    {
+        readonly SQLiteAsyncConnection _database;
+        public AgendaDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Agenda>().Wait();
+        }
+        public Task<List<Agenda>> GetNotesAsync()
+        {
+            return _database.Table<Agenda>().ToListAsync();
+        }
+        public Task<Agenda> GetNoteAsync(int id)
+        {
+            return _database.Table<Agenda>()
+            .Where(i => i.ID == id)
+            .FirstOrDefaultAsync();
+        }
+        public Task<int> SaveNoteAsync(Agenda note)
+        {
+            if (note.ID != 0)
+            {
+                return _database.UpdateAsync(note);
+            }
+            else
+            {
+                return _database.InsertAsync(note);
+            }
+        }
+        public Task<int> DeleteNoteAsync(Agenda note)
+        {
+            return _database.DeleteAsync(note);
+        }
+    }
+
+}
---
+++ PersonalAgenda/NotePage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonalAgenda.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PersonalAgenda
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NotePage : ContentPage
+    {
+        public NotePage()
+        {
+            InitializeComponent();
+        }
+    }
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var note = (Agenda)BindingContext;
+        note.Date = DateTime.UtcNow;
+        await App.Database.SaveNoteAsync(note);
+        await Navigation.PopAsync();
+    }
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var note = (Agenda)BindingContext;
+        await App.Database.DeleteNoteAsync(note);
+        await Navigation.PopAsync();
+    }
+}
